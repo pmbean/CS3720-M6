b98a683f6dbe54c2d4e8ec532bac87ee8818c5bb
Patrick Bean <pmbean@live.com>
2019-04-19 22:10:06 -0400

    Initial Commit

---
+++ HearMeNow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HearMeNow
+//
+//  Created by Patrick Bean on 4/19/19.
+//  Copyright © 2019 TheDevBean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
